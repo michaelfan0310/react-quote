febbbe7b2404edbcbeb58cce96efeddb40d1f78a
marshall2023 <74441675+michaelfan0310@users.noreply.github.com>
2022-07-24 11:51:17 -0400

    7-24-1

--- src/components/ButtonWrap.js
+++ src/components/ButtonWrap.js
@@ -7,14 +7,13 @@ function ButtonWrap(){
   <div className="buttonWrapper">
    <>
     <Button variant="outline-light" href="https://michaelfan0310.github.io/react-quote/">React</Button>{' '} 
-     <Button variant="outline-light" href="https://michaelfan0310.github.io/special/aquarium">Special</Button>{' '} 
+     <Button variant="outline-light" href="https://michaelfan0310.github.io/special/aquarium">Special</Button>{' '}
+     <Button variant="outline-light" href="https://www.michaelfan0310.ml/vue_music">Vue</Button>{' '} 
      <Button variant="outline-light" href="https://www.michaelfan0310.ml/library/">Library</Button>{' '}  
 
      <Button variant="outline-light" href="https://michaelfan0310.github.io/news/pandemic.html">News</Button>{' '} 
 
      <Button variant="outline-light" href="https://michaelfan0310.github.io/michael2020/CSSPhotoGallery.html">Moments</Button>{' '}  
-
-  <Button variant="outline-light" href="https://michaelfan0310.github.io/travel/videoWeb.html">Travel</Button>{' '}  
 
   <Button variant="outline-light" href="https://michaelfan0310.github.io/blog/simon/">Game</Button>{' '}   
     
