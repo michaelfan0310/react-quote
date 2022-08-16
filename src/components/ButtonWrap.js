d98b01ce37c28bfced546fc72a245e10e679e7dd
marshall2023 <74441675+michaelfan0310@users.noreply.github.com>
2022-08-15 23:14:33 -0400

    8-15-1

--- src/components/ButtonWrap.js
+++ src/components/ButtonWrap.js
@@ -6,16 +6,16 @@ function ButtonWrap(){
   return(
   <div className="buttonWrapper">
    <>
-    <Button variant="outline-light" href="https://michaelfan0310.github.io/react-quote/">React</Button>{' '} 
-     <Button variant="outline-light" href="https://michaelfan0310.github.io/special/aquarium">Special</Button>{' '}
+    <Button variant="outline-light" href="https://www.michaelfan0310.ml/react-quote/">React</Button>{' '} 
+     <Button variant="outline-light" href="https://www.michaelfan0310.github.ml/special/aquarium">Special</Button>{' '}
      <Button variant="outline-light" href="https://www.michaelfan0310.ml/vue_music">Vue</Button>{' '} 
      <Button variant="outline-light" href="https://www.michaelfan0310.ml/library/">Library</Button>{' '}  
 
-     <Button variant="outline-light" href="https://michaelfan0310.github.io/news/pandemic.html">News</Button>{' '} 
+     <Button variant="outline-light" href="https://www.michaelfan0310.ml/news/pandemic.html">News</Button>{' '} 
 
-     <Button variant="outline-light" href="https://michaelfan0310.github.io/michael2020/CSSPhotoGallery.html">Moments</Button>{' '}  
+     <Button variant="outline-light" href="https://www.michaelfan0310.ml/michael2020/CSSPhotoGallery.html">Moments</Button>{' '}  
 
-  <Button variant="outline-light" href="https://michaelfan0310.github.io/blog/simon/">Game</Button>{' '}   
+  <Button variant="outline-light" href="https://www.michaelfan0310.ml/blog/simon/">Game</Button>{' '}   
     
    </>   
     </div>
